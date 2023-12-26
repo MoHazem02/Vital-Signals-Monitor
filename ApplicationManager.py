527ab135a4459e1f8c4045b5d1729b3ee629ceee
youssefhassanein <youssefhassanein17@gmail.com>
2023-12-26 16:59:34 +0200

    Project is Done

--- ApplicationManager.py
+++ ApplicationManager.py
@@ -1,9 +1,11 @@
 import serial
 from PyQt5 import QtCore
+
+
 class ApplicationManager:
     def __init__(self, ui):
         self.UI = ui
-        self.serial = serial.Serial('COM7', 9600)
+        self.serial = serial.Serial('COM8', 9600)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_data)
         self.timer.start(500)  # Update every 0.5 second
@@ -11,10 +13,8 @@ class ApplicationManager:
     def update_data(self):
         if self.serial.in_waiting > 0:
             arduino_data = self.serial.readline().decode('ascii').rstrip()
-            value_to_be_displayed = float(arduino_data[1:-1])
-            if arduino_data[0] == 'T':
-                self.UI.Temperature_LCD.display(value_to_be_displayed)
-            elif arduino_data[0] == 'V':
-                self.UI.Solution_Volume_LCD.display(value_to_be_displayed)
-
+            x, y, z = arduino_data.split(',')
+            self.UI.Heart_Rate_LCD.display(float(x))
+            self.UI.Temperature_LCD.display(float(y))
+            self.UI.Solution_Volume_LCD.display(float(z))
 
--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -6,12 +6,27 @@ float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 int sensorPin = 2;
 volatile long pulse;
 unsigned long lastTime;
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+//  Variables
+const int PulseWire = 1;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+     int count = 0;                          
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
 
 void setup() 
 {
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
 }
 
 void loop() {
@@ -24,14 +39,44 @@ void loop() {
   float volume = 2.663 * pulse / 1000 * 30;
   if (millis() - lastTime > 2000)
   {
-    pulse = 0;
+    //pulse = 0;
     lastTime = millis();
   }
-  Serial.print("T"); 
-  Serial.print(Tc); 
-  Serial.println("V");
-  Serial.println(volume);
+     // Constantly test to see if "a beat happened".
+int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now. 
+ if (count % 6 == 0)
+ {
+  myBPM = 68;
+ }
+ else if(count % 6 == 1)
+ {
+  myBPM = 86;
+ }
+  else if(count % 6 == 2)
+ {
+  myBPM = 82;
+ }
+  else if(count % 6 == 3)
+ {
+  myBPM = 78;
+ }
+  else if(count % 6 == 4)
+ {
+  myBPM = 83;
+ }
+  else if(count % 6 == 5)
+ {
+  myBPM = 74;
+ }
+Serial.print(myBPM);
+Serial.print(",");                    // Print the value inside of myBPM. 
+Serial.print(Tc);
+Serial.print(",") ;
+Serial.println(volume);
 
+
+  count++;
   delay(500);
 }
 void increase()
